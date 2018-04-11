7fe4cf9ad35c55ba459b7f0a7fa6e4b964ab3758
Ankush Choubey <anks.choubey@gmail.com>
2018-04-11 20:20:26 +0530

    Initial Commit

---
+++ group_by_sum_mapper.py
@@ -0,0 +1,26 @@
+import sys
+import csv
+# input comes from STDIN (standard input)
+for line in sys.stdin:
+    # remove leading and trailing whitespace
+    line = line.strip()
+    # split the line into words
+
+    #print(len(line.split(',')))
+
+    field_data =  csv.reader([line],delimiter =',')
+    field_data = list(field_data)[0]
+
+    # increase counters
+
+    key_column, aggregate_column = sys.argv[1:]
+
+    key_column, aggregate_column = int(key_column), int(aggregate_column)
+
+    try:
+        # if first line
+        int(field_data[aggregate_column])
+    except:
+        continue
+
+    print(field_data[key_column], '\t',field_data[aggregate_column])
